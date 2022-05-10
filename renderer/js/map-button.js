6e09b96a8a6c717f8961800257faf4e7d3b2b46c
Jacob Calvert <jacob.calvert@windriver.com>
2022-05-10 11:49:29 -0500

    added map button class for reset and other buttons, fixed sprite anchor so rotation is around sprite center

---
+++ renderer/js/map-button.js
@@ -0,0 +1,45 @@
+
+
+class MapButton{
+
+    constructor(btnUpImage, btnDownImage, x, y, onClick)
+    {
+
+        this.txt_up = PIXI.Texture.from(btnUpImage);
+        this.txt_down = PIXI.Texture.from(btnDownImage);
+        this.sprite = PIXI.Sprite.from(this.txt_up);
+        this.sprite.width = 100;
+        this.sprite.height = 100;
+        this.sprite.anchor.set(0.5); // set anchor to 50% so rotations occur around the center
+        this.sprite.x = x;
+        this.sprite.y = y;
+        this.on_click = onClick;
+        this.sprite.buttonMode = true;
+        this.sprite.interactive = true;
+        this.sprite.on('pointerdown', () => this.buttonDown());
+        this.sprite.on('pointerup', () => this.buttonUp());
+
+    }
+    
+    click()
+    {
+        this.on_click();
+    }
+
+
+    buttonDown()
+    {
+        this.sprite.texture = this.txt_down;
+        this.sprite.alpha = 1.0;
+        this.click();
+    }
+
+    buttonUp()
+    {
+        this.sprite.texture = this.txt_up;
+    }
+
+
+    
+
+}
