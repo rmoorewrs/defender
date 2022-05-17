fb1a5d6334b1bbbc870dcd4bf279bd6a9bfaba8b
Rich Moore <rmoorewrs@gmail.com>
2022-05-17 14:28:28 -0400

    fix start_example_drivers.sh

--- OCI/drivers/scripts/start_example_driver.sh
+++ OCI/drivers/scripts/start_example_driver.sh
@@ -3,6 +3,6 @@
 python3 driver-attack.py $OBJECT_NAME $SERVER_IP:$SERVER_PORT
 
 # do a reset on exit
-if [$RESET = true]; then
-    curl http://10.0.2.15:30824/v1/commands/reset -X POST
+if [ $RESET == true ]; then
+    curl http://$SERVER_IP:$SERVER_PORT/v1/commands/reset -X POST
 fi
