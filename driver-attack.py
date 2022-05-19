43b07b9db2f31f3a1b4d7db07f635a64f04c7922
Rich Moore <rmoorewrs@gmail.com>
2022-05-18 20:14:45 -0400

    in the middle of modifying driver-attack.py

--- driver-attack.py
+++ driver-attack.py
@@ -1,3 +1,5 @@
+from ast import Pass
+from asyncore import poll
 from flask_restful import abort
 import requests
 import json
@@ -6,42 +8,100 @@ import math
 import sys
 import server_ip_multicast
 
-API_PATH='/v1/objects/name/'
-URL=''
+# set some parameters
+MAX_SPEED=10    # maximum speed we can move
+DELTA_T=0.25    # main loop period
+RETRY_PERIOD=10 # delay if we're not connecting to server
+GOAL=[{'x':295},{'y':50}]
+DEFAULT_SERVER_ADDRESS='http://worldserver:5000'
+OBJECT_API_PATH='/v1/objects/name/'
+SENSOR_API_PATH='/v1/sensors/name'
+SCANRANGE=200
+OBJECT_NAME=''
+OBJECT_STATE=None
+SENSOR_STATE=None
+OBJECT_STATE_URL=''
+SENSOR_STATE_URL=''
 
-def get_state() -> dict:
-    rsp = requests.get(URL)
-    state = json.loads(rsp.text)
-    return state
+def get_object_state() -> dict:
+    try:
+        rsp = requests.get(OBJECT_STATE_URL)
+
+        state = json.loads(rsp.text)
+        return state
+    except:
+        print(f'failed to connect to server {OBJECT_STATE_URL}')
+        return None
+    
+def get_sensor_state() -> dict:
+    try:
+        rsp = requests.get(OBJECT_STATE_URL)
+        sensor_scan = json.loads(rsp.text)
+        return sensor_scan
+    except:
+        print(f'failed to connect to server {SENSOR_STATE_URL}')
+        return None
 
 def set_state(state):
-    requests.put(URL, data=state)
+    requests.put(OBJECT_STATE_URL, data=state)
 
-step_state = 0
-dstep = math.pi/180.0
+def wait_for_server(poll_period=RETRY_PERIOD)->dict:
+    state = get_object_state()
+    if state:
+        return state
+    else:    
+        while state is None:
+            time.sleep(poll_period)
+            state=get_object_state()
+            
+
+def set_initial_state(state, speed=MAX_SPEED)->dict:
+    # check to see if speed has already been set, otherwise set it
+    # speed, position and rotation are state variables and should remain the same
+    if state['speed']==0:
+        state['speed']=speed
+    return state
 
 def modify_state(state) -> dict:
     global step_state, dstep
-    R = 200
     
-    base_x = 500
-    base_y = 500
-    base_rotation = -180 
+    max_move = MAX_SPEED * DELTA_T
 
-    dx = R*math.cos(step_state)
-    dy = R*math.sin(step_state)
+    x = state['x']
+    y = state['y']
+    angle=math.radians(float(state['rotation'])-90.0)
 
-    state["x"] = str(int(base_x + dx))
-    state["y"] = str(int(base_y + dy))
-    state["rotation"] = str(int(base_rotation + math.degrees(step_state)))
+    dx = max_move*math.cos(angle)
+    dy = max_move*math.sin(angle)
 
-    step_state += dstep
+    state["x"] = x + dx
+    state["y"] = y + dy
     
-
     return state 
 
+
+def sense_environment(state,sensors):
+    global OBJECT_STATE,SENSOR_STATE
+    
+    return
+
+def plan_path():
+    global OBJECT_STATE
+    pass
+
+def execute_plan():
+    global OBJECT_STATE
+    pass
+
+def abort_if_dead():
+    if OBJECT_STATE['status'] == 'dead':
+        sys.exit({"status":"Object is Dead"})
+
+def abort_if_outofbounds():
+    pass
+
 def usage_and_exit(app_name):
-    print (app_name + " <object_name> [server IP:port | multicast]")
+    print (f"{app_name} <object_name> [server IP:port | multicast]")
     print ("<object_name> must be a valid object")
     sys.exit()
 
@@ -50,32 +110,38 @@ if __name__ == "__main__":
         usage_and_exit(sys.argv[0])
 
     # get name of object to move around in world-model 
-    object_name=sys.argv[1]
+    OBJECT_NAME=sys.argv[1]
 
     # check for IP or multicast
-    server_address="http://worldserver:5000"  # set default worldserver address
+    server_address=DEFAULT_SERVER_ADDRESS  # set default worldserver address
     if len(sys.argv) == 3:
         if sys.argv[2] == 'multicast':
             server_address=server_ip_multicast.wait_forever_for_server_address()
         else:
             server_address='http://' + sys.argv[2]
         
-    URL= server_address + API_PATH + object_name
-    print ("Connecting to server at " + server_address + " using full API path " + URL)
-    
-    while True:
-        retry_period=5
-        state={'state':'init'}
-        try:
-            state = get_state()
-            state = modify_state(state)
-            set_state(state)
-            time.sleep(0.25)
-        except:
-            print(f'failed to connect to server {URL} retrying in {retry_period} seconds')
-            time.sleep(retry_period)
+    OBJECT_STATE_URL= server_address + OBJECT_API_PATH + OBJECT_NAME
+    SENSOR_STATE_URL= server_address + SENSOR_API_PATH + OBJECT_NAME + "?scanrange=" + SCANRANGE
+    print (f"{OBJECT_NAME} driver connecting to {server_address} using full API path {OBJECT_STATE_URL}")
+    print (f"{OBJECT_NAME} scanning from {server_address} using full API path {SENSOR_STATE_URL}")
 
-        if state['state'] == 'dead':
-            print (sys.argv[1] + " died due to collision!")
-            sys.exit({"status":"Object is Dead"})
+    # get initial state, initial sensor readings and set object speed
+    wait_for_server()
+    OBJECT_STATE=get_object_state()
+    SENSOR_STATE=get_sensor_state()
+    OBJECT_STATE=set_initial_state(OBJECT_STATE,MAX_SPEED)
+    
+
+    while True:
+        OBJECT_STATE=get_object_state(OBJECT_STATE)
+        if OBJECT_STATE:
+            sense_environment()
+            plan_path()
+            execute_plan()
+            abort_if_outofbounds()
+            abort_if_dead()
+    
+        time.sleep(DELTA_T)
+
+
 
