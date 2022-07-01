98c062d6e612c5cd449a770734b5fb59327c3b5d
Rich Moore <rmoorewrs@gmail.com>
2022-07-01 11:57:07 -0400

    convenience scripts to run attackers and defenders

---
+++ run_defenders.sh
@@ -0,0 +1,3 @@
+python3 driver-defender.py config/defender01.json 10.10.11.56:5000 &
+python3 driver-defender.py config/defender02.json 10.10.11.56:5000 &
+python3 driver-defender.py config/defender03.json 10.10.11.56:5000 &
