d71d12cc327138e7bfaca687d2c15a17f8c46198
Rich Moore <rmoorewrs@gmail.com>
2022-07-13 12:53:51 -0400

    update to configmaps

--- OCI/kubernetes/create-configmaps.sh
+++ OCI/kubernetes/create-configmaps.sh
@@ -1,8 +1,8 @@
 # create configmaps for each pod
 kubectl create configmap configmap-defender-server --from-file=configmap/defender-server.properties
-kubectl create configmap configmap-attacker01 --from-file=configmap/defender-server.properties --from-env-file=configmap/attacker01-env.properties
-kubectl create configmap configmap-attacker02 --from-file=configmap/defender-server.properties --from-env-file=configmap/attacker02-env.properties
-kubectl create configmap configmap-attacker03 --from-file=configmap/defender-server.properties --from-env-file=configmap/attacker03-env.properties
-kubectl create configmap configmap-defender01 --from-file=configmap/defender-server.properties --from-env-file=configmap/defender01-env.properties
-kubectl create configmap configmap-defender02 --from-file=configmap/defender-server.properties --from-env-file=configmap/defender02-env.properties
-kubectl create configmap configmap-defender03 --from-file=configmap/defender-server.properties --from-env-file=configmap/defender03-env.properties
+kubectl create configmap configmap-attacker01 --from-env-file=configmap/attacker01-env.properties
+kubectl create configmap configmap-attacker02 --from-env-file=configmap/attacker02-env.properties
+kubectl create configmap configmap-attacker03 --from-env-file=configmap/attacker03-env.properties
+kubectl create configmap configmap-defender01 --from-env-file=configmap/defender01-env.properties
+kubectl create configmap configmap-defender02 --from-env-file=configmap/defender02-env.properties
+kubectl create configmap configmap-defender03 --from-env-file=configmap/defender03-env.properties
